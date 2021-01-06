5aea3029140a4d970114307607c18a205ea85ef8
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2021-01-06 03:58:38 -0300

    toDoListOK

--- src/pages/ToDoList/index.jsx
+++ src/pages/ToDoList/index.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import CheckBox from '@react-native-community/checkbox';
 
-import { Wrapper, ContainerList } from './styles';
+import {
+  Wrapper,
+  ContainerList,
+  List,
+  ContainerData,
+  Title,
+  Description,
+  Date,
+} from './styles';
+
+import api from '../../services/api';
 
 export default function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [isSelected, setSelection] = useState(false);
+
+  useEffect(() => {
+    api.get('/tasks').then((response) => {
+      setTasks(response.data);
+    });
+  }, []);
+
   return (
     <Wrapper>
-      <ContainerList />
+      <ContainerList>
+        {tasks.map((task) => (
+          <List key={task.id}>
+            <CheckBox
+              value={isSelected}
+              onValueChange={setSelection}
+              style={styles.checkbox}
+            />
+            <ContainerData>
+              <Title>{task.title}</Title>
+              <Description>{task.description}</Description>
+              <Date>{task.date}</Date>
+            </ContainerData>
+          </List>
+        ))}
+      </ContainerList>
     </Wrapper>
   );
 }
+
+const styles = StyleSheet.create({
+  checkbox: {
+    marginLeft: 19,
+    alignSelf: 'center',
+  },
+});
--- src/pages/ToDoList/styles.js
+++ src/pages/ToDoList/styles.js
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.SafeAreaView``;
+export const Wrapper = styled.SafeAreaView`
+  flex: 1;
+`;
 
-export const ContainerList = styled.View``;
+export const ContainerList = styled.View`
+  width: 100%;
+  height: 100%;
+  background: #f5f5f5;
+  align-items: center;
+`;
+
+export const List = styled.View`
+  width: 95%;
+  height: 85px;
+  background: #fff;
+  margin-top: 15px;
+  border-radius: 15px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ContainerData = styled.View`
+  flex-direction: column;
+  margin-left: 17px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const Description = styled.Text`
+  font-size: 14px;
+  color: #888;
+`;
+
+export const Date = styled.Text`
+  font-size: 14px;
+  color: #888;
+`;
